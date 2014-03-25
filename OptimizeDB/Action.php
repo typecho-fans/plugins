508165a672ed0e2d5b9e32dc6e1f33961c11cf2e
ShingChi <shingchi@sina.cn>
2014-03-26 00:24:29 +0800

    精简下语句

--- OptimizeDB/Action.php
+++ OptimizeDB/Action.php
@@ -4,33 +4,26 @@
  *
  * @copyright  Copyright (c) 2014 Binjoo (http://binjoo.net)
  * @license    GNU General Public License 2.0
- * 
+ *
  */
 
 class OptimizeDB_Action extends Typecho_Widget implements Widget_Interface_Do
 {
-    private $db;
-
-    public function __construct($request, $response, $params = NULL)
+    public function optimize()
     {
-        parent::__construct($request, $response, $params);
-        $this->db = Typecho_Db::get();
-    }
-    public function optimize(){
-        $config = Typecho_Db::get()->getConfig();
-        $dblist = $this->db->fetchAll("SHOW TABLE STATUS FROM " . $config[0]->database);
-        foreach($dblist as $row){
-            $result = $this->db->fetchAll('OPTIMIZE TABLE ' . $row['Name']);
+        $db = Typecho_Db::get();
+        $config = $db->getConfig();
+        $tables = $db->fetchAll("SHOW TABLE STATUS FROM " . $config[0]->database);
+        foreach ($tables as $row) {
+            $result = $db->fetchAll('OPTIMIZE TABLE ' . $row['Name']);
         }
         $this->widget('Widget_Notice')->set(_t('数据库优化完成！'), 'success');
         $this->response->goBack();
     }
 
-    public function action(){
-        if(!$this->widget('Widget_User')->pass('administrator')){
-            throw new Typecho_Widget_Exception(_t('禁止访问'), 403);
-        }
+    public function action()
+    {
+        $this->widget('Widget_User')->pass('administrator');
         $this->on($this->request->is('optimize'))->optimize();
     }
 }
-?>
