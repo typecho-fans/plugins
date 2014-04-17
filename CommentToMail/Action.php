9b55133a79191841ed51bdea41eb6d83bbf2b1c0
byends <zwxiang@qq.com>
2014-04-17 17:54:13 +0800

    更新至 1.3.2

--- CommentToMail/Action.php
+++ CommentToMail/Action.php
@@ -14,7 +14,7 @@ class CommentToMail_Action extends Typecho_Widget implements Widget_Interface_Do
     private $_cfg;
     private $_isMailLog = false;
     public  $mailer;
-    public  $smtp;
+    public  $email;
 
     /**
      * 读取缓存文件内容，并根据条件组合邮件内容发送。
@@ -31,9 +31,9 @@ class CommentToMail_Action extends Typecho_Widget implements Widget_Interface_Do
         //获取评论内容
         $file = $this->_dir . '/cache/' . $fileName;
         if (file_exists($file)) {
-            $this->smtp = unserialize(file_get_contents($file));
-            if (!$this->widget('Widget_User')->simpleLogin($this->smtp->ownerId)) {
-                @unlink($file);
+            $this->email = unserialize(file_get_contents($file));
+            @unlink($file);
+            if (!$this->widget('Widget_User')->simpleLogin($this->email->ownerId)) {
                 $this->widget('Widget_Archive@404', 'type=404')->render();
                 exit;
             }
@@ -43,8 +43,8 @@ class CommentToMail_Action extends Typecho_Widget implements Widget_Interface_Do
         }
         
         //如果本次评论设置了拒收邮件，把coid加入拒收列表
-        if ($this->smtp->banMail) {
-            $this->proveParent($this->smtp->coid, 1);
+        if ($this->email->banMail) {
+            $this->proveParent($this->email->coid, 1);
         }
 
         //选择发信模式
@@ -57,81 +57,84 @@ class CommentToMail_Action extends Typecho_Widget implements Widget_Interface_Do
                 break;
             case 'smtp':
                 $this->mailer->IsSMTP();
+                
                 if (in_array('validate', $this->_cfg->validate)) {
                     $this->mailer->SMTPAuth = true;
                 }
+                
                 if (in_array('ssl', $this->_cfg->validate)) {
                     $this->mailer->SMTPSecure = "ssl";
                 }
+                
                 $this->mailer->Host     = $this->_cfg->host;
                 $this->mailer->Port     = $this->_cfg->port;
                 $this->mailer->Username = $this->_cfg->user;
                 $this->mailer->Password = $this->_cfg->pass;
-                $this->smtp->from     = $this->_cfg->user;
+                $this->email->from      = $this->_cfg->user;
                 break;
         }
 
         //向博主发邮件的标题格式
-        $this->smtp->titleForOwner = $this->_cfg->titleForOwner;
+        $this->email->titleForOwner = $this->_cfg->titleForOwner;
         
         //向访客发邮件的标题格式
-        $this->smtp->titleForGuest = $this->_cfg->titleForGuest;
+        $this->email->titleForGuest = $this->_cfg->titleForGuest;
 
         //验证博主是否接收自己的邮件
-        $toMe = (in_array('to_me', $this->_cfg->other) && $this->smtp->ownerId == $this->smtp->authorId) ? true : false;
+        $toMe = (in_array('to_me', $this->_cfg->other) && $this->email->ownerId == $this->email->authorId) ? true : false;
 
         //向博主发信
-        if (in_array($this->smtp->status, $this->_cfg->status) && in_array('to_owner', $this->_cfg->other)
-            && ( $toMe || $this->smtp->ownerId != $this->smtp->authorId) && 0 == $this->smtp->parent ) {
+        if (in_array($this->email->status, $this->_cfg->status) && in_array('to_owner', $this->_cfg->other)
+            && ( $toMe || $this->email->ownerId != $this->email->authorId) && 0 == $this->email->parent ) {
             if (empty($this->_cfg->mail)) {
-            	Typecho_Widget::widget('Widget_Users_Author@' . $this->smtp->cid, array('uid' => $this->smtp->ownerId))->to($user);
-            	$this->smtp->to = $user->mail;
+                Typecho_Widget::widget('Widget_Users_Author@temp' . $this->email->cid, array('uid' => $this->email->ownerId))->to($user);
+            	$this->email->to = $user->mail;
             } else {
-                $this->smtp->to = $this->_cfg->mail;
+                $this->email->to = $this->_cfg->mail;
             }
 
             $this->sendMail('owner');
         }
 
         //向访客发信
-        if (0 != $this->smtp->parent && 'approved' == $this->smtp->status && in_array('to_guest', $this->_cfg->other)
-            && $this->proveParent($this->smtp->parent)
-        ) {
+        if (0 != $this->email->parent 
+            && 'approved' == $this->email->status 
+            && in_array('to_guest', $this->_cfg->other)
+            && $this->proveParent($this->email->parent)) {
+            
             //如果联系我的邮件地址为空，则使用文章作者的邮件地址
-            if (empty($this->smtp->contactme)) {
-                Typecho_Widget::widget('Widget_Users_Author@' . $this->smtp->cid, array('uid' => $this->smtp->ownerId))->to($user);
-                $this->smtp->contactme = $user->mail;
+            if (empty($this->email->contactme)) {
+                if (!isset($user) || !$user) {
+                    Typecho_Widget::widget('Widget_Users_Author@temp' . $this->email->cid, array('uid' => $this->email->ownerId))->to($user);
+                }
+                $this->email->contactme = $user->mail;
             } else {
-                $this->smtp->contactme = $this->_cfg->contactme;
+                $this->email->contactme = $this->_cfg->contactme;
             }
 
             $original = $this->_db->fetchRow($this->_db->select('author', 'mail', 'text')
                                                        ->from('table.comments')
-                                                       ->where('coid = ?', $this->smtp->parent));
+                                                       ->where('coid = ?', $this->email->parent));
 
-            $toGuest = (!in_array('to_me',$this->_cfg->other) && $this->smtp->mail == $original['mail'] || $this->smtp->to == $original['mail']) ? false : true;
-
-            if ($toGuest) {
-                $this->smtp->to             = $original['mail'];
-                $this->smtp->originalText   = $original['text'];
-                $this->smtp->originalAuthor = $original['author'];
+            if (in_array('to_me', $this->_cfg->other) 
+                || $this->email->mail != $original['mail']) {
+                $this->email->to             = $original['mail'];
+                $this->email->originalText   = $original['text'];
+                $this->email->originalAuthor = $original['author'];
                 $this->sendMail('guest');
             }
         }
 
         $this->mailLog(false, "邮件发送完毕！\n");
-
-        @unlink($file);
-
     }
     
     /*
      * 生成邮件内容并发送
-     * $sendTo 为  0 发向博主;  1 发向访客
+     * $sendTo 为  owner 发向博主;  其它发向访客
      */
     public function sendMail($author = 'owner')
     {
-    	$date = new Typecho_Date($this->smtp->created);
+    	$date = new Typecho_Date($this->email->created);
         $time = date('Y-m-d H:i:s', $date->timeStamp);
 
         if ('owner' == $author) {
@@ -155,16 +158,16 @@ class CommentToMail_Action extends Typecho_Widget implements Widget_Interface_Do
                 '{status}'
             );
             $replace = array(
-                $this->smtp->site,
-                $this->smtp->title,
-                $this->smtp->author,
-                $this->smtp->ip,
-                $this->smtp->mail,
-                $this->smtp->permalink,
-                $this->smtp->manage,
-                $this->smtp->text,
+                $this->email->site,
+                $this->email->title,
+                $this->email->author,
+                $this->email->ip,
+                $this->email->mail,
+                $this->email->permalink,
+                $this->email->manage,
+                $this->email->text,
                 $time,
-                $status[$this->smtp->status]
+                $status[$this->email->status]
             );
         } else {
             $subject = $this->_cfg->titleForGuest;
@@ -182,42 +185,42 @@ class CommentToMail_Action extends Typecho_Widget implements Widget_Interface_Do
                 '{time}'
             );
             $replace = array(
-                $this->smtp->site,
-                $this->smtp->title,
-                $this->smtp->originalAuthor,
-                $this->smtp->author,
-                $this->smtp->mail,
-                $this->smtp->permalink,
-                $this->smtp->text,
-                $this->smtp->contactme,
-                $this->smtp->originalText,
+                $this->email->site,
+                $this->email->title,
+                $this->email->originalAuthor,
+                $this->email->author,
+                $this->email->mail,
+                $this->email->permalink,
+                $this->email->text,
+                $this->email->contactme,
+                $this->email->originalText,
                 $time
             );
         }
 
-        $this->smtp->body = str_replace($search, $replace, $body);
-        $this->smtp->subject = str_replace($search, $replace, $subject);
-        $this->smtp->AltBody = "作者：".$this->smtp->author."\r\n链接：".$this->smtp->permalink."\r\n评论：\r\n".$this->smtp->text;
+        $this->email->body = str_replace($search, $replace, $body);
+        $this->email->subject = str_replace($search, $replace, $subject);
+        $this->email->AltBody = "作者：".$this->email->author."\r\n链接：".$this->email->permalink."\r\n评论：\r\n".$this->email->text;
 
-        $this->mailer->SetFrom($this->smtp->from, $this->smtp->site);
-        $this->mailer->AddReplyTo($this->smtp->to, $this->smtp->site);
-        $this->mailer->Subject = $this->smtp->subject;
-        $this->mailer->AltBody = $this->smtp->AltBody;
-        $this->mailer->MsgHTML($this->smtp->body);
+        $this->mailer->SetFrom($this->email->from, $this->email->site);
+        $this->mailer->AddReplyTo($this->email->to, $this->email->site);
+        $this->mailer->Subject = $this->email->subject;
+        $this->mailer->AltBody = $this->email->AltBody;
+        $this->mailer->MsgHTML($this->email->body);
 
-        $name = $this->smtp->originalAuthor ? $this->smtp->originalAuthor : $this->smtp->site;
+        $name = $this->email->originalAuthor ? $this->email->originalAuthor : $this->email->site;
 
-        $this->mailer->AddAddress($this->smtp->to,$name);
+        $this->mailer->AddAddress($this->email->to,$name);
 
         if ($this->mailer->Send()) {
             $this->mailLog();
         } else {
             $this->mailLog(false);
         }
+        
         $this->mailer->ClearAddresses();
         $this->mailer->ClearReplyTos();
     }
-
 
     /*
      * 记录邮件发送日志和错误信息
@@ -231,7 +234,7 @@ class CommentToMail_Action extends Typecho_Widget implements Widget_Interface_Do
         $fileName = $this->_dir . '/log/mailer_log.txt';
         if ($type) {
             $content  = $content ? $content : date("Y-m-d H:i:s",
-                            $this->smtp->created + $this->smtp->timezone) . " 向 " . $this->smtp->to . " 发送邮件成功！\r\n";
+                            $this->email->created + $this->email->timezone) . " 向 " . $this->email->to . " 发送邮件成功！\r\n";
         } else {
             $content  = $content ? $content : $this->mailer->ErrorInfo;
         }
@@ -279,7 +282,6 @@ class CommentToMail_Action extends Typecho_Widget implements Widget_Interface_Do
             } else {
                 return true;
             }
-
         } else {
             return false;
         }
