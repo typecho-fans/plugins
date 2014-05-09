1eefb6cd19a0d8f3bc9c5cbf0365fa5ee608705b
chekun <234267695@qq.com>
2014-05-09 15:21:53 +0800

    fix route error

--- market.php
+++ market.php
@@ -1,3 +1,3 @@
 <?php !defined('__TYPECHO_ROOT_DIR__') and exit();
 
-Typecho_Response::getInstance()->redirect('app-store/market');
+Typecho_Response::getInstance()->redirect('/index.php/admin/app-store/market');
