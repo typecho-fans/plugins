498d21d45f851c3917b39d6ee4e876b4e8b4ced0
shuxiao9058 <shuxiao9058@gmail.com>
2014-09-02 00:52:09 +0800

    add plugin gtCaptcha

---
+++ gtCaptcha/Plugin.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * gtCaptcha验证码插件
+ * 
+ * @package gtCaptcha
+ * @author 啸傲居士
+ * @version 1.0.0
+ * @link http://jiya.io
+ */
+
+require_once('SDK/geetestlib.php');
+
+class gtCaptcha_Plugin implements Typecho_Plugin_Interface
+{
+
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     * 
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate() {
+		Typecho_Plugin::factory('Widget_Feedback')->comment = array(__CLASS__, 'filter');
+    }
+    
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     * 
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate() {}
+    
+    /**
+     * 个人用户的配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form) {}
+    
+    /**
+     * 获取插件配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+	public static function config(Typecho_Widget_Helper_Form $form) {
+		$publickeyDescription = _t("To use GeeTest you must get an API key from <a href='http://www.geetest.com/'>http://www.geetest.com/</a>");
+		$publickey = new Typecho_Widget_Helper_Form_Element_Text('publickey', NULL, '', _t('Public Key:'), $publickeyDescription);
+		$privatekey = new Typecho_Widget_Helper_Form_Element_Text('privatekey', NULL, '', _t('Private Key:'), _t(''));
+		
+		$form->addInput($publickey);
+		$form->addInput($privatekey);
+	}
+	
+	/**
+	 * 展示验证码
+	 */
+	public static function output() {
+		$publickey = Typecho_Widget::widget('Widget_Options')->plugin('gtCaptcha')->publickey;
+		
+		echo geetest_get_html($publickey);
+	}
+  
+	public static function filter($comment, $obj) {
+		$privatekey = Typecho_Widget::widget('Widget_Options')->plugin('gtCaptcha')->privatekey;
+		$challenge = $_POST['geetest_challenge'];
+		$validate = $_POST['geetest_validate'];
+		$seccode = $_POST['geetest_seccode'];
+		
+		$geetest_response = geetest_check_answer($privatekey, $challenge, $validate, $seccode);
+		$userObj = $obj->widget('Widget_User');
+		
+		if($userObj->hasLogin() && $userObj->pass('administrator', true)) {
+			return $comment;
+		}
+		
+		if (!$geetest_response) {
+			throw new Typecho_Widget_Exception(_t('验证码不正确哦！'));
+		}
+		
+		return $comment;
+	}
+}
---
+++ gtCaptcha/README.md
@@ -0,0 +1,25 @@
+gtCaptcha验证码插件
+=============
+防止垃圾评论，作者「[啸傲居士](http://jiya.io)」。
+
+
+在[seccode代码](http://521-wf.com/archives/36.html)的基础上修改的验证码插件，可以到[官方体验页面](http://geetest.com/tryit/)体验。
+
+
+### 使用说明
+
+1. 在[GeeTest官网](http://my.geetest.com/)页面申请Public Key和Private Key；
+2. 激活并配置完key后在适当的地方添加如下代码：
+
+```
+gtCaptcha_Plugin::output();
+```
+
+### 附：
+
+为方便大家，特提供如下key：
+
+```
+Public Key: 6d1d522f9af8576c4287bde5d1963047
+Private Key: 88aa2a14010d795a3d27d9f24fec4ba6
+```
