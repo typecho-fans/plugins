ca62193242aeda6c2466d7ed80b824b475ab29e7
zhulin3141 <zhulin3141@gmail.com>
2014-11-15 20:39:36 +0800

    add install function

--- TeStore/Action.php
+++ TeStore/Action.php
@@ -1,5 +1,7 @@
 <?php ! defined('__TYPECHO_ROOT_DIR__') and exit();
 
+include_once 'unzip.php';
+
 class TeStore_Action extends Typecho_Widget {
 
     private $server;
@@ -8,6 +10,8 @@ class TeStore_Action extends Typecho_Widget {
     private $cacheTime;
 
     private $cacheDir;
+
+    private $pluginRoot;
 
     private $pluginInfo;
 
@@ -19,6 +23,10 @@ class TeStore_Action extends Typecho_Widget {
         $this->server = $pluginOpts->server;
         $this->cacheTime = $pluginOpts->cache_time;
         $this->cacheDir = dirname(__FILE__) . '/data/';
+        $this->pluginRoot = __TYPECHO_ROOT_DIR__ . __TYPECHO_PLUGIN_DIR__ . '/';
+
+        $this->getAppData();
+
         define('TYPEHO_ADMIN_PATH', __TYPECHO_ROOT_DIR__ . __TYPECHO_ADMIN_DIR__.'/');
     }
 
@@ -74,7 +82,7 @@ class TeStore_Action extends Typecho_Widget {
      */
     private function parseRemoteData()
     {
-        $html = file_get_contents($this->server);
+        @$html = file_get_contents($this->server);
         preg_match_all("/<a href=\"(.+zip).+\">(.+?)<\/a>.+\s*<td.*>(.+?)<\/td>\s*<td>(.+?)<\/td>\s*<td><a href=\"(.+)\">(.+)<\/a><\/td>\s*<td><a href=\"(.+)\">(.+)<\/a>/", $html, $this->pluginInfo);
         array_shift($this->pluginInfo);
 
@@ -99,7 +107,7 @@ class TeStore_Action extends Typecho_Widget {
         $fieldName = array('zipFile', 'pluginName', 'desc', 'version', 'site', 'author', 'pluginUrl', 'source');
         foreach( range(0, count($this->pluginInfo[0]) - 1 ) as $plugIdx ){
             foreach ( range(0, $fieldNum - 1 ) as $fieldIdx) {
-                $pluginData[$plugIdx][$fieldName[$fieldIdx]] = $this->pluginInfo[$fieldIdx][$plugIdx];
+                $pluginData[$plugIdx]->$fieldName[$fieldIdx] = $this->pluginInfo[$fieldIdx][$plugIdx];
             }
         }
 
@@ -115,19 +123,82 @@ class TeStore_Action extends Typecho_Widget {
         file_put_contents($this->cacheDir . time() . '.json', $pluginInfo);
     }
 
+    private function getActivePlugins()
+    {
+        $activatedPlugins = Typecho_Plugin::export();
+        return array_keys( $activatedPlugins['activated'] );
+    }
+
+    /**
+     * 获取目录下的文件夹
+     *
+     * @access private
+     * @return array
+     */
+    private static function getDir($targetDir)  
+    {
+        $dirs = array();
+        $files = scandir($targetDir);
+        foreach($files as $file){
+            if( is_dir($targetDir . '/' . $file) && !in_array($file, array('.', '..')) ){
+                $dirs[] = $file;
+            }
+        }
+        return $dirs;
+    }
+
     public function market()
     {
-        $pluginInfo = $this->getAppData();
-        if( $pluginInfo ){
-            $activatedPlugins = Typecho_Plugin::export();
-            $activatedPlugins = array_keys( $activatedPlugins['activated'] );
-        }
+        $options = Helper::options();
+        $pluginPath = Typecho_Common::url('TeStore', $options->pluginUrl);
+        $pluginInfo = $this->pluginInfo;
+        $activatedPlugins = $this->getActivePlugins();
+        $installPlugins = self::getDir($this->pluginRoot);
 
         include_once 'views/market.php';
     }
 
     public function install()
     {
-        
+        $plugin  = $this->request->get('plugin');
+        $pluginPath = $this->pluginRoot . '/' . $plugin . '/';
+        $pluginInfo = $this->getPluginByName($plugin);
+        $tempFile = dirname(__FILE__) . "/.tmp/" . $pluginInfo->pluginName . '.zip';
+        $activatedPlugins = $this->getActivePlugins();
+
+        $ret = array(
+            'status' => false,
+            'error' => '',
+        );
+
+        if( in_array($plugin, $activatedPlugins) ){
+            $ret['error'] = '无法安装已经安装的插件';
+        }else{
+            if( false !== $pluginInfo ){
+                $zipFile = file_get_contents($pluginInfo->zipFile);
+                file_put_contents( $tempFile, $zipFile );
+
+                @mkdir($pluginPath);
+                $unzip = new Unzip();
+                if( ! $unzip->extract($tempFile, $this->pluginRoot) ){
+                    $ret['error'] = $unzip->error_string();
+                }else{
+                    $ret['status'] = true;
+                }
+
+                chmod($tempFile, 0755);
+                @unlink($tempFile);
+                echo json_encode($ret);
+            }
+        }
+    }
+
+    public function getPluginByName($name)
+    {
+        foreach ($this->pluginInfo as $plugin) {
+            if( $plugin->pluginName == $name )
+                return $plugin;
+        }
+        return false;
     }
 }
