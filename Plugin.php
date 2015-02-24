b5f6359c30752a934c054cca09e425f83be7ee50
chekun <234267695@qq.com>
2015-02-24 20:57:37 +0800

    ready for 1.2.0

--- Plugin.php
+++ Plugin.php
@@ -4,7 +4,7 @@
  *
  * @package AppStore
  * @author chekun
- * @version 1.1.1
+ * @version 1.2.0
  * @link http://typecho.dilicms.com
  */
 class AppStore_Plugin implements Typecho_Plugin_Interface
