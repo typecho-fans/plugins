6cb1336b55db7ba19dac0099ce1b197edcb6f611
lanxi <lanxi@mogujie.com>
2017-01-16 20:31:34 +0800

    匹配 typecho 默认主题

--- views/market.php
+++ views/market.php
@@ -6,26 +6,26 @@
     list($version, $buildVersion) = explode('/', Typecho_Common::VERSION);
     $typechoVersion = floatval($version);
 ?>
-<link rel="stylesheet" href="<?php echo $options->pluginUrl('AppStore/static/css/font-awesome.min.css'); ?>">
-<link rel="stylesheet" href="<?php echo $options->pluginUrl('AppStore/static/css/pure.css'); ?>">
 <style>
-    .button-small {
-        font-size: 85%;
+    .as-name,
+    .as-require,
+    .as-author,
+    .as-operations {
+        white-space: nowrap;
     }
 </style>
 <?php if ($typechoVersion <= 0.8): ?>
     <div class="main">
-        <div class="body body-950">
-            <div class="container typecho-page-title">
+        <div class="body container">
+            <div class="typecho-page-title">
                 <div class="column-24">
                     <h2><?php echo $menu->title; ?> <small><cite>The missing plugins' store for Typecho</cite></small></h2>
                     <div>
-                        <i class="fa fa-heart" title="<?php echo _t('提建议/吐槽专用'); ?>"></i>
-                        <a href="http://chekun.me/typecho-app-store.html" target="_blank"><?php echo _t('提建议/吐槽专用'); ?></a>
+                        <a href="https://github.com/chekun/AppStore/issues" target="_blank"><?php echo _t('提建议/吐槽专用'); ?></a>
                     </div>
                 </div>
             </div>
-            <div class="container typecho-page-main">
+            <div class="row typecho-page-main" role="main">
                 <?php include 'list.php'; ?>
             </div>
         </div>
@@ -33,23 +33,20 @@
 <?php else: ?>
     <div class="main">
         <div class="body container">
-            <div class="colgroup">
-                <div class="typecho-page-title col-mb-12">
-                    <h2>
-                        <?php echo $menu->title; ?> <small><cite>The missing plugins' store for Typecho</cite></small>
-                        <div style="float:right">
-                            <a href="http://chekun.me/typecho-app-store.html" target="_blank"><i class="fa fa-heart" title="<?php echo _t('提建议/吐槽专用'); ?>"></i><?php echo _t('提建议/吐槽专用'); ?></a>
-                        </div>
-                    </h2>
-                </div>
+            <div class="typecho-page-title">
+                <h2>
+                    <?php echo $menu->title; ?> <small><cite>The missing plugins' store for Typecho</cite></small>
+                    <div style="float:right">
+                        <a href="https://github.com/chekun/AppStore/issues" target="_blank"><?php echo _t('提建议/吐槽专用'); ?></a>
+                    </div>
+                </h2>
             </div>
-            <div class="colgroup typecho-page-main" role="main">
+            <div class="row typecho-page-main" role="main">
                 <?php include 'list.php'; ?>
             </div>
         </div>
     </div>
 <?php endif; ?>
-
 <?php
     include TYPEHO_ADMIN_PATH.'copyright.php';
     include TYPEHO_ADMIN_PATH.'common-js.php';
