66079fc8251523ff5a4297ffd6082af3250768e8
jzwalk <zyz59@126.com>
2017-01-17 19:28:35 +0800

    Squashed 'AppStore/' changes from ee1ba29..09b4504
    09b4504 merge with develop
    b147be2 Merge pull request #28 from aiwb/develop
    6cb1336 匹配 typecho 默认主题
    c90f7c5 添加 @aiwb 到 Contributors 列表
    fc81442 Merge pull request #27 from aiwb/develop
    6d87941 使用 table 展现插件列表
    
    git-subtree-dir: AppStore
    git-subtree-split: 09b45047c8fa60b5e0dd6afca7c77b439f261334

--- views/market.php
+++ views/market.php
@@ -6,40 +6,26 @@
     list($version, $buildVersion) = explode('/', Typecho_Common::VERSION);
     $typechoVersion = floatval($version);
 ?>
-<link rel="stylesheet" href="<?php echo $options->pluginUrl('AppStore/static/css/font-awesome.min.css'); ?>">
-<link rel="stylesheet" href="<?php echo $options->pluginUrl('AppStore/static/css/pure.css'); ?>">
 <style>
-    .as-description {
-        height: 4.2em;
-        overflow: hidden;
-    }
-    .as-status {
-        float:right;
-        margin-right: 1em;
-    }
-    .as-status i {
-        color: #ccc;
-        margin: 0 0.2em;
-        font-size: 1.5em;
-    }
-    .as-status i.active {
-        color: green
+    .as-name,
+    .as-require,
+    .as-author,
+    .as-operations {
+        white-space: nowrap;
     }
 </style>
-
 <?php if ($typechoVersion <= 0.8): ?>
     <div class="main">
-        <div class="body body-950">
-            <div class="container typecho-page-title">
+        <div class="body container">
+            <div class="typecho-page-title">
                 <div class="column-24">
                     <h2><?php echo $menu->title; ?> <small><cite>The missing plugins' store for Typecho</cite></small></h2>
-                    <p> 
-                        <i class="fa fa-heart" title="<?php echo _t('提建议/吐槽专用'); ?>"></i>
-                        <a href="http://chekun.me/typecho-app-store.html" target="_blank"><?php echo _t('提建议/吐槽专用'); ?></a>
-                    </p>
+                    <div>
+                        <a href="https://github.com/chekun/AppStore/issues" target="_blank"><?php echo _t('提建议/吐槽专用'); ?></a>
+                    </div>
                 </div>
             </div>
-            <div class="container typecho-page-main">
+            <div class="row typecho-page-main" role="main">
                 <?php include 'list.php'; ?>
             </div>
         </div>
@@ -47,23 +33,20 @@
 <?php else: ?>
     <div class="main">
         <div class="body container">
-            <div class="colgroup">
-                <div class="typecho-page-title col-mb-12">
-                    <h2>
-                        <?php echo $menu->title; ?> <small><cite>The missing plugins' store for Typecho</cite></small>
-                        <p style="float:right"> 
-                            <a href="http://chekun.me/typecho-app-store.html" target="_blank"><i class="fa fa-heart" title="<?php echo _t('提建议/吐槽专用'); ?>"></i><?php echo _t('提建议/吐槽专用'); ?></a>
-                        </p>
-                    </h2>
-                </div>
+            <div class="typecho-page-title">
+                <h2>
+                    <?php echo $menu->title; ?> <small><cite>The missing plugins' store for Typecho</cite></small>
+                    <div style="float:right">
+                        <a href="https://github.com/chekun/AppStore/issues" target="_blank"><?php echo _t('提建议/吐槽专用'); ?></a>
+                    </div>
+                </h2>
             </div>
-            <div class="colgroup typecho-page-main" role="main">
+            <div class="row typecho-page-main" role="main">
                 <?php include 'list.php'; ?>
             </div>
         </div>
     </div>
 <?php endif; ?>
-
 <?php
     include TYPEHO_ADMIN_PATH.'copyright.php';
     include TYPEHO_ADMIN_PATH.'common-js.php';
