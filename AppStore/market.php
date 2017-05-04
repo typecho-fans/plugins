bc22af871514a46f2b4e05b9d85e8acd1f3795fa
车坤 <234267695@qq.com>
2017-05-04 12:28:24 +0800

    :q

--- AppStore/market.php
+++
@@ -1,8 +0,0 @@
-<?php !defined('__TYPECHO_ROOT_DIR__') and exit();
-
-$options = Helper::options();
-$siteUrl = $options->siteUrl;
-$isRewrite = $options->rewrite;
-$absUrl = ($isRewrite ? rtrim($siteUrl, '/') : $siteUrl."index.php");
-
-Typecho_Response::getInstance()->redirect($absUrl.__TYPECHO_ADMIN_DIR__.'app-store/market');
