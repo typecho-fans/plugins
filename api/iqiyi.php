eb93dbccfad6fe09e7318d9c70d18e441336fc65
jzwalk <zyz59@126.com>
2018-06-24 23:05:09 +0800

    Squashed 'MyPlayer/' content from commit a0f72e6
    git-subtree-dir: MyPlayer
    git-subtree-split: a0f72e623ff319c17c83d6855c6bf3674ed23da8

---
+++ api/iqiyi.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * 爱奇艺
+ */
+class iqiyi extends API{
+    protected function LoadRemote( ) {
+        if( TryGetParam( 'id', $id ) ) {
+            $html = GetUrlContent( "http://www.iqiyi.com/v_{$id}.html" );
+            $count = preg_match_all( '/\"vid\":\"([0-9a-z]+)\"/s', $html, $matchs );
+            if( $count ){
+                $vid = $matchs[1][0];
+                $this->data = array( );
+                $this->data['url'] = "http://player.video.qiyi.com/{$vid}/0/0/v_{$id}.swf";
+            }
+        }
+    }
+}
