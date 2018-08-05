8124b511fa3ed67f1cbbbd00d61894ffa5d36712
jzwalk <zyz59@126.com>
2018-08-05 19:17:42 +0800

    Squashed 'ShortLinks/' changes from 844d05a..d2cde16
    d2cde16 修正面板问题，兼容Typecho1.0+
    f824969 修复PHP7.2下数组下标找不到
    4e3813a 修复方法找不到
    64c76d6 新增自定义字段处理（实验性功能）
    b018d39 修改版本号
    3358874 修复不能用
    2ac113a 修复自定义锻炼功能导致评论链接转换错误
    61033dc 修复自定义链接功能
    10015e6 精简代码
    c5b3c32 修改变量名
    ebf5e57 更新README
    25972e6 新增跳转页面 修复bug 精简代码 增加注释
    28439b2 紧急修复referer白名单失效
    094070f 紧急修复白名单失效
    
    git-subtree-dir: ShortLinks
    git-subtree-split: d2cde16ca7f18c5a73f4c2ad22f434a7b0a0d4b3

--- Plugin.php
+++ Plugin.php
@@ -6,7 +6,7 @@
  *
  * @package ShortLinks
  * @author Ryan
- * @version 1.0.7
+ * @version 1.0.9
  * @link http://blog.iplayloli.com/typecho-plugin-shortlinks.html
  */
  class ShortLinks_Plugin implements Typecho_Plugin_Interface
@@ -44,7 +44,9 @@
 		Helper::addPanel(2, 'ShortLinks/panel.php', '短链接', '短链接管理',   'administrator');
 		Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('ShortLinks_Plugin','replace');
 		Typecho_Plugin::factory('Widget_Abstract_Contents')->excerptEx = array('ShortLinks_Plugin','replace');
+		Typecho_Plugin::factory('Widget_Abstract_Contents')->filter = array('ShortLinks_Plugin','replace');
 		Typecho_Plugin::factory('Widget_Abstract_Comments')->filter = array('ShortLinks_Plugin','replace');
+		Typecho_Plugin::factory('Widget_Archive')->singleHandle = array('ShortLinks_Plugin','replace');
 		return('数据表 '.$shortlinks.' 创建成功, 插件已经成功激活!');
 	}
 	/**
@@ -75,11 +77,17 @@
 		$form->addInput($radio);
 		$radio =  new Typecho_Widget_Helper_Form_Element_Radio('convert_comment_link' , array('1'=>_t('开启'),'0'=>_t('关闭')),'1',_t('转换评论者链接'),_t('开启后会帮你把评论者链接转换成内链'));
 		$form->addInput($radio);
+		$radio =  new Typecho_Widget_Helper_Form_Element_Radio('go_page' , array('1'=>_t('开启'),'0'=>_t('关闭')),'1',_t('跳转页面开关'),_t('开启后会展示跳转页面'));
+		$form->addInput($radio);
+		$edit =  new Typecho_Widget_Helper_Form_Element_Text('go_delay', NULL, _t('3'), _t('跳转延时'), _t('跳转页面停留时间'));
+		$form->addInput($edit);
 		$radio =  new Typecho_Widget_Helper_Form_Element_Radio('target' , array('1'=>_t('开启'),'0'=>_t('关闭')),'1',_t('新窗口打开文章中的链接'),_t('开启后会帮你文章中的链接新增target属性'));
 		$form->addInput($radio);
-		$refererList =  new Typecho_Widget_Helper_Form_Element_Textarea('refererList', NULL, NULL, _t('referer 白名单'), _t('在这里设置 referer 白名单，一行一个'));
-		$form->addInput($refererList);
-		$nonConvertList =  new Typecho_Widget_Helper_Form_Element_Textarea('nonConvertList', NULL, _t("b0.upaiyun.com" . PHP_EOL ."glb.clouddn.com" . PHP_EOL ."qbox.me" . PHP_EOL ."qnssl.com"), _t('外链转换白名单'), _t('在这里设置外链转换白名单'));
+		$textarea =  new Typecho_Widget_Helper_Form_Element_Textarea('convert_custom_field', NULL, NULL, _t('需要处理的自定义字段'), _t('在这里设置需要处理的自定义字段，一行一个(实验性功能)'));
+		$form->addInput($textarea);
+		$referer_list =  new Typecho_Widget_Helper_Form_Element_Textarea('referer_list', NULL, NULL, _t('referer 白名单'), _t('在这里设置 referer 白名单，一行一个'));
+		$form->addInput($referer_list);
+		$nonConvertList =  new Typecho_Widget_Helper_Form_Element_Textarea('nonConvertList', NULL, _t("b0.upaiyun.com" . PHP_EOL ."glb.clouddn.com" . PHP_EOL ."qbox.me" . PHP_EOL ."qnssl.com"), _t('外链转换白名单'), _t('在这里设置外链转换白名单(评论者链接不生效)'));
 		$form->addInput($nonConvertList);
 	}
 	/**
@@ -98,35 +106,66 @@
 	 * @param $class
 	 * @return $content
 	 */
-	public static function replace($text, $widget, $lastResult) {
-		$rewrite = (Helper::options()->rewrite) ? '' : 'index.php/'; // 伪静态处理
-		$pOption = Typecho_Widget::widget('Widget_Options')->Plugin('ShortLinks'); // 插件选项
-		$target  = ($pOption->target) ? ' target="_blank" ' : ''; // 新窗口打开
-		$nonConvertList = self::textareaToArr($pOption->nonConvertList); // 不转换列表
-		if($pOption->convert == 1)  {
-			$text = empty($lastResult) ? $text : $lastResult;
-			if (($widget instanceof Widget_Archive)||($widget instanceof Widget_Abstract_Comments)) {
-				$options = Typecho_Widget::widget('Widget_Options');
-				@preg_match_all('/<a(.*?)href="(.*?)"(.*?)>/',$text,$matches);
-				if($matches){
-					foreach($matches[2] as $val){
-						if (strpos($val,'://')!==false && strpos($val,rtrim($options->siteUrl, '/')) !== false) continue; // 本站链接
-						if (self::checkDomain($val, $nonConvertList)) continue; // 不转换列表中的不处理
-						if (preg_match('/\.(jpg|jepg|png|ico|bmp|gif|tiff)/i',$val)) continue; // 图片不处理
-						$text=str_replace("href=\"$val\"", "href=\"".$options->siteUrl. $rewrite ."go/".str_replace("/","|",base64_encode(htmlspecialchars_decode($val)))."\"" . $target, $text);
+	public static function replace($text, $widget) {
+		$pluginOption = Typecho_Widget::widget('Widget_Options')->Plugin('ShortLinks'); // 插件选项
+		$siteUrl = Helper::options()->siteUrl;
+		$target  = ($pluginOption->target) ? ' target="_blank" ' : ''; // 新窗口打开
+		if($pluginOption->convert == 1)  {
+			if (!is_string($text) && $text instanceof Widget_Archive) {
+			// 自定义字段处理
+				$fieldsList = self::textareaToArr($pluginOption->convert_custom_field);
+				if ($fieldsList) {
+					foreach ($fieldsList as $field) {
+						if (isset($text->fields[$field])) {
+							@preg_match_all('/<a(.*?)href="(.*?)"(.*?)>/',$text->fields[$field], $matches);
+							if($matches){
+								foreach($matches[2] as $link){
+									$text->fields[$field] = str_replace("href=\"$link\"", "href=\"". self::convertLink($link) . "\"", $text->fields[$field]);
+								}
+							}
+						}
 					}
 				}
 			}
-			if ($pOption->convert_comment_link == 1) {
-				if ($widget instanceof Widget_Abstract_Comments) {
-					$url = $text['url'];
-					if(strpos($url,'://')!==false && strpos($url, rtrim($options->siteUrl, '/'))===false) {
-						$text['url'] = $options->siteUrl . $rewrite ."go/".str_replace("/","|",base64_encode(htmlspecialchars_decode($url)));
+			if (($widget instanceof Widget_Archive)||($widget instanceof Widget_Abstract_Comments)) {
+			// 文章内容和评论内容处理
+				@preg_match_all('/<a(.*?)href="(.*?)"(.*?)>/', $text, $matches);
+				if($matches){
+					foreach($matches[2] as $link){
+						$text= str_replace("href=\"$link\"", "href=\"". self::convertLink($link) . "\"" . $target, $text);
 					}
+				}
+			}
+			if ($pluginOption->convert_comment_link == 1 && $widget instanceof Widget_Abstract_Comments) {
+			// 评论者链接处理
+				$url = $text['url'];
+				if(strpos($url,'://')!==false && strpos($url, rtrim($siteUrl, '/'))===false) {
+					$text['url'] = self::convertLink($url, false);
 				}
 			}
 		}
 		return $text;
+	}
+	/**
+	 * 转换链接形式
+	 *
+	 * @access public
+	 * @param $link
+	 * @return $string
+	 */
+	public static function convertLink($link, $check = true) {
+		$rewrite = (Helper::options()->rewrite) ? '' : 'index.php/'; // 伪静态处理
+		$pluginOption = Typecho_Widget::widget('Widget_Options')->Plugin('ShortLinks'); // 插件选项
+		$linkBase = ltrim(rtrim(Typecho_Router::get('go')['url'] , '/'), '/'); // 防止链接形式修改后不能用
+		$siteUrl = Helper::options()->siteUrl;
+		$target  = ($pluginOption->target) ? ' target="_blank" ' : ''; // 新窗口打开
+		$nonConvertList = self::textareaToArr($pluginOption->nonConvertList); // 不转换列表
+		if ($check) {
+			if (strpos($link, '://') !== false && strpos($link, rtrim($siteUrl, '/')) !== false) return $link; //本站链接不处理
+			if (self::checkDomain($link, $nonConvertList)) return $link; // 不转换列表中的不处理
+			if (preg_match('/\.(jpg|jepg|png|ico|bmp|gif|tiff)/i',$link)) return $link; // 图片不处理
+		}
+		return $siteUrl . $rewrite. str_replace('[key]', str_replace("/","|",base64_encode(htmlspecialchars_decode($link))), $linkBase);
 	}
 	/**
 	 * 检查域名是否在数组中存在
@@ -156,7 +195,7 @@
 	 */
 	public static function textareaToArr($textarea) {
 		$str = str_replace(array("\r\n", "\r", "\n"), "|", $textarea);
-		if ($str ="") return null;
-		$arr = explode("|", $str);
+		if ($str == "") return null;
+		return explode("|", $str);
 	}
  }
--- README.md
+++ README.md
@@ -2,24 +2,19 @@
 有时候我们为了减少权重的流失，或者是为了隐藏某些推荐链接（比如淘宝客、主机推荐），我们需要将外链转化为内链（淘宝客、主机推荐都是是隐藏AFF）。
 
 Typecho外链转内链插件，支持正文和评论者链接
-
 最稳定版下载地址:https://github.com/benzBrake/ShortLinks/releases/latest
 ### 简介
-~~把外部链接转换为 your_blog_path/t/key/ 或者 your_blog_path/go/key/；
+1.把外部链接转换为your_blog_path/go/key/，撰写链接页面支持修改
+2.通过菜单“创建->短链接”设置；
+3.自定义短链功能来自[golinks](http://defe.me/prg/429.html "golinks")；
+4.支持 referer 白名单和外链转换白名单。
+5.支持跳转页面
+6.支持自定义字段转换（实验性功能）
 
-已经合并为 your_blog_path/go/key/；
-
-通过菜单“创建->短链接”设置；
-
-自定义短链功能来自[golinks](http://defe.me/prg/429.html "golinks")；
-
-支持 referer 白名单和外链转换白名单。
 ### 使用方法 Uasge
 ### Todo
 
-1. 增加跳转页面
-2. 自定义短连接增加密码功能
-3. 支持处理更多部分的链接
+1. 自定义短连接增加密码功能
 
 ### 感谢
 [小咪兔](http://forum.typecho.org/viewtopic.php?t=5576 "小咪兔")
