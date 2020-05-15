aa0f518c060f0eb86442cad1732f7a9be33b9001
Jasper.Z <zyz59@126.com>
2020-05-15 12:28:26 +0800

    Squashed 'GoodLuck/' changes from f99c4b8..7d3aebb
    7d3aebb 修正随机算法不随机的问题
    
    git-subtree-dir: GoodLuck
    git-subtree-split: 7d3aebb7fd0699d6e9761f8c4f2775a8c25dc5c7

--- Plugin.php
+++ Plugin.php
@@ -4,11 +4,10 @@
  *
  * @package GoodLuck
  * @author Ryan
- * @version 1.0.1
+ * @version 1.0.2
  * @link http://blog.iplayloli.com/typecho-plugin-goodluck
  */
- class GoodLuck_Plugin implements Typecho_Plugin_Interface
- {
+ class GoodLuck_Plugin implements Typecho_Plugin_Interface {
 	 /**
 	 * execute function.
 	 *
@@ -23,8 +22,7 @@
 	 * @return String
 	 * @throws Typecho_Plugin_Exception
 	 */
-	public static function activate()
-	{
+	public static function activate() {
 		Helper::addRoute('goodluck', '/goodluck', 'GoodLuck_Plugin', 'goodluck');
 		return('插件已经成功激活!');
 	}
@@ -36,8 +34,7 @@
 	 * @return String
 	 * @throws Typecho_Plugin_Exception
 	 */
-	public static function deactivate()
-	{
+	public static function deactivate() {
 		Helper::removeRoute('goodluck');
 	}
 	/**
@@ -47,8 +44,7 @@
 	 * @param Typecho_Widget_Helper_Form $form 配置面板
 	 * @return void
 	 */
-	public static function config(Typecho_Widget_Helper_Form $form)
-	{
+	public static function config(Typecho_Widget_Helper_Form $form) {
 	}
 	/**
 	 * 个人用户的配置面板
@@ -79,16 +75,43 @@
 		$result = $db->fetchAll($sql);
 		$min_id = $result[0]['MIN(`cid`)'];//POST类型数据最小的CID
 		$result = NULL;
+		$rand_ids = Typecho_Cookie::get('contents_rand_ids');//获取最近5篇随机展示文章ID
+		if (empty($rand_ids)) {
+			$rand_ids = array();
+		} else {
+			$rand_ids = explode(',', $rand_ids);
+		}
+		$times_out = 0;//计算循环次数
+		$target = Typecho_Widget::widget('Widget_Options')->siteUrl;//默认跳转首页
 		while($result == NULL) {
+			$times_out++;//循环计数
 			$rand_id = mt_rand($min_id,$max_id);
+			//查询数据
 			$sql = $db->select()->from('table.contents')
 				->where('status = ?','publish')
 				->where('type = ?', 'post')
 				->where('created <= unix_timestamp(now())', 'post')
 				->where('cid = ?',$rand_id);
 			$result = $db->fetchAll($sql);
+			if (in_array($rand_id, $rand_ids)) {
+				$result = NULL;
+			} else {
+				if($result !=NULL) {
+					$rand_ids = array_push($rand_ids, $rand_id);
+					if (count($rand_ids) == 5) {
+						unset($rand_ids[0]);
+						$rand_ids = array_values($rand_ids);
+					}
+					$rand_ids = implode(',', $rand_ids);
+					Typecho_Cookie::set("contents_rand_ids",$rand_ids);
+					$target = Typecho_Widget::widget('Widget_Abstract_Contents')->filter($result['0']);
+				}
+			}
+			//超过100次不干了
+			if ($times_out > 100)
+				break;
 		}
-		$target = Typecho_Widget::widget('Widget_Abstract_Contents')->filter($result['0']);
+		header("Cache-Control: no-store, no-cache, must-revalidate");
 		Typecho_Response::redirect($target['permalink'], 307);
 	}
- }
+}
