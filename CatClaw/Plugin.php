f725dadc9d880c9bbdcca7eab84bff9d18ac66fc
jrotty <bssf@qq.com>
2021-01-15 16:16:44 +0800

    更新1.5.3
    1，之前版采集时无标签时会默认标签为待定，此版本采集时遇到无标签的时候就默认不填写标签
    2，修改下作者链接，原域名以换主，此次更新的作者链接因为备案原因暂时也没法访问不过备案完成后就正常了

--- CatClaw/Plugin.php
+++ CatClaw/Plugin.php
@@ -6,8 +6,8 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  * 
  * @package CatClaw
  * @author jrotty
- * @version 1.5.2
- * @link https://qqdie.com/archives/typecho-CatClaw.html
+ * @version 1.5.3
+ * @link https://zezeshe.com/archives/typecho-CatClaw.html
  */
 class CatClaw_Plugin implements Typecho_Plugin_Interface
 {
